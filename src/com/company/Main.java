168cd6c8651953900d31fd155c0c474eb387c0b3
oaleksander <medvedev.asm@outlook.com>
2021-09-17 10:54:55 +0300

    it works

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,38 +5,36 @@ import static java.lang.Math.sqrt;
 public class Main {
 
     public static void main(String[] args) {
-        double v_n, p_n, i_n, p1, u1, i1, u2, i2, z_k, r_k, x_k, r1, x1, u1__, p2, cos1, cos2, sin1, sin2, sign;
+        double v_n, p_n, i_n, p1k, u1k, i1k, u20, i2k, z_k, r_k, x_k, r1, x1, u1__, p2, cos1, cos2, sin1, sin2, sign;
         sign = 1;
-        cos1 = 0.7;
+        cos1 = 0.9;
         v_n = 220;
-        p_n = 93.3;
+        p_n = 78.7;
         i_n = p_n/v_n;
+        System.out.println(i_n);
 
-        p1 = 7.75;
-        u1 = 29.76;
-        i1 = i_n;
+        p1k = 6.65;
+        u1k = 27.61;
+        i1k = 0.346;
+        i2k = 0.62;
 
-        z_k = u1/i1;
-        r_k = p1/(i1*i1);
+        z_k = u1k/i1k;
+        r_k = p1k/(i1k*i1k);
         x_k = sqrt(z_k*z_k - r_k*r_k);
 
-        r1 = r_k/2;
-        x1 = x_k/2;
 
-        u1__ = 220.45;
-        p2 = 3.45;
-        i2 = 0.048;
-        u2 = 115.56;
+        u20 = 120.81;
 
-        cos2 = p1/(i1*u1);
+        cos2 = p1k/(i1k*u1k);
         sin1 = sqrt(1-cos1*cos1);
         sin2 = sqrt(1 - cos2*cos2);
         sin2 = sign*sin2;
 
-        double result = u2 - u1*(cos1*cos2 + sin1*sin2);
+        double result = u20 - u1k*(cos1*cos2 + sin1*sin2)/(((i2k/i1k)+(v_n/u20))/2);
 
         System.out.println(result);
-        System.out.println(r1 + "xuy" + x1);
+        System.out.println();
+        //System.out.println(r1 + "xuy" + x1);
 
 
     }
